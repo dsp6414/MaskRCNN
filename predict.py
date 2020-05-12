a2f7018398c5f2cdfd668f05097bd9897b2f464a
AkashAVI <61403402+AkashAVI@users.noreply.github.com>
2020-05-12 15:00:29 +0530

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,124 @@
+import os
+import numpy as np
+import torch
+import torch.utils.data
+from torchvision.transforms import ToTensor
+from PIL import Image
+import torchvision
+from dataset import BasicDataset
+import utils
+import argparse
+import logging
+import torch.nn.functional as F
+from torchvision import transforms
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
+
+#########################################################################
+def predict_img(model,
+                img,
+                device):
+    model.eval()
+    with torch.no_grad():
+        prediction = model([img.to(device)])
+
+    result = Image.fromarray(prediction[0]['masks'][0, 0].mul(255).byte().cpu().numpy())
+
+    return result
+ ########################################################################   
+def get_args():
+    parser = argparse.ArgumentParser(description='Predict masks from input images',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--model', '-m', default='MODEL.pth',
+                        metavar='FILE',
+                        help="Specify the file in which the model is stored")
+    parser.add_argument('--input', '-i', metavar='INPUT', nargs='+',
+                        help='directory of input image', required=True)
+
+    parser.add_argument('--output', '-o', metavar='INPUT', nargs='+',
+                        help='Filenames of ouput images')
+    parser.add_argument('--viz', '-v', action='store_true',
+                        help="Visualize the images as they are processed",
+                        default=False)
+    parser.add_argument('--no-save', '-n', action='store_true',
+                        help="Do not save the output masks",
+                        default=False)
+
+    return parser.parse_args()
+#########################################################################
+
+def get_output_filenames(args):
+    in_files = args.input
+    out_files = []
+
+    if not args.output:
+        for f in in_files:
+            pathsplit = os.path.splitext(f)
+            out_files.append("{}_OUT{}".format(pathsplit[0], pathsplit[1]))
+    elif len(in_files) != len(args.output):
+        logging.error("Input files and output files are not of the same length")
+        raise SystemExit()
+    else:
+        out_files = args.output
+
+    return out_files
+#########################################################################
+def get_instance_segmentation_model(num_classes):
+    # load an instance segmentation model pre-trained on COCO
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=False)
+    # get the number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # now get the number of input features for the mask classifier
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+    # and replace the mask predictor with a new one
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+                                                       hidden_layer,
+                                                       num_classes)
+
+    return model
+##########################################################################
+
+if __name__ == "__main__":
+    args = get_args()
+    in_files = args.input
+    out_files = get_output_filenames(args)
+
+    logging.info("Loading model {}".format(args.model))
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    logging.info(f'Using device {device}')
+
+    num_classes = 2 ###### no. of classes in the image
+    model = get_instance_segmentation_model(num_classes)  
+    model.to(device=device)
+    model.load_state_dict(torch.load(args.model, map_location=device))
+
+    logging.info("Model loaded !")
+
+    for i, fn in enumerate(in_files):
+        logging.info("\nPredicting image {} ...".format(fn))
+
+        # img, _ = dataset_test[i]
+        image= Image.open('/content/download.png').convert("RGB")
+        img = ToTensor()(image)
+        mask = predict_img(model=model,
+                           img=img,
+                           device=device)
+
+        if not args.no_save:
+            out_fn = out_files[i]
+            result = mask
+            result.save(out_files[i])
+
+            logging.info("Mask saved to {}".format(out_files[i]))
+
+        if args.viz:
+            logging.info("Visualizing results for image {}, close to continue ...".format(fn))
+            plot_img_and_mask(img, mask)
+
+      
